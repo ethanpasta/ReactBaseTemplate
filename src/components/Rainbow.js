f8b5250f43fc106f89f2480b1de628543daf27f5
Eitan Mayer <40837133+ethanpasta@users.noreply.github.com>
2020-03-12 18:23:42 +0200

    Change 'class' to 'className'

--- src/components/Rainbow.js
+++ src/components/Rainbow.js
@@ -3,14 +3,14 @@ import '../styles/Rainbow.scss';
 
 const Rainbow = ({ text }) => {
     return (
-        <ul class="c-rainbow">
-            <li class="c-rainbow__layer c-rainbow__layer--white">{text}</li>
-            <li class="c-rainbow__layer c-rainbow__layer--orange">{text}</li>
-            <li class="c-rainbow__layer c-rainbow__layer--red">{text}</li>
-            <li class="c-rainbow__layer c-rainbow__layer--violet">{text}</li>
-            <li class="c-rainbow__layer c-rainbow__layer--blue">{text}</li>
-            <li class="c-rainbow__layer c-rainbow__layer--green">{text}</li>
-            <li class="c-rainbow__layer c-rainbow__layer--yellow">{text}</li>
+        <ul className="c-rainbow">
+            <li className="c-rainbow__layer c-rainbow__layer--white">{text}</li>
+            <li className="c-rainbow__layer c-rainbow__layer--orange">{text}</li>
+            <li className="c-rainbow__layer c-rainbow__layer--red">{text}</li>
+            <li className="c-rainbow__layer c-rainbow__layer--violet">{text}</li>
+            <li className="c-rainbow__layer c-rainbow__layer--blue">{text}</li>
+            <li className="c-rainbow__layer c-rainbow__layer--green">{text}</li>
+            <li className="c-rainbow__layer c-rainbow__layer--yellow">{text}</li>
         </ul>
     )
 };
